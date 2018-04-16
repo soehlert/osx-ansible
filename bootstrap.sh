3c37f943f88a90577a5f94aa1b9caf981057fd0f
Sam Oehlert <(none)>
2018-04-15 23:48:43 -0500

    add bootstrap file

---
+++ bootstrap.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+# shell script to bootstrap the system to the point that it has ansible
+if [ ! -d /Library/Developer ]; then
+    echo Insalling Xcode first, then rerun this script
+    sleep 1
+    xcode-select --install
+else
+   /bin/bash ./roles/homebrew/files/install-homebrew.sh
+   /usr/local/bin/brew install ansible
+fi
