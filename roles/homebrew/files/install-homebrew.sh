170c41eb4baef8c2c3317f16a715ad9e9a250b33
Sam Oehlert <sam.oehlert@gmail.com>
2020-08-18 22:27:13 -0500

    refactor(homebrew-install): use new bash script for homebrew install instead of the old ruby method
    ruby script is deprecated

--- roles/homebrew/files/install-homebrew.sh
+++ roles/homebrew/files/install-homebrew.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-yes | /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+yes | /usr/bin/ruby -e "$(curl -fsSL
+https://raw.githubusercontent.com/Homebrew/install/master/install)"
