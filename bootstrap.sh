0d6a23fea85caf97a9aaa4e93da6b297671aa55a
Sam Oehlert <sam.oehlert@gmail.com>
2020-08-20 14:50:16 -0500

    refactor(xcode): homebrew non interactively installs xcode so let's just install homebrew and let it do its magic
    https://github.com/Homebrew/install/blob/878b5a18b89ff73f2f221392ecaabd03c1e69c3f/install#L297

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,11 +1,5 @@
 #!/bin/sh
 
 # shell script to bootstrap the system to the point that it has ansible
-if [ ! -d /Library/Developer ]; then
-    echo "Insalling Xcode first, then rerun this script"
-    sleep 1
-    xcode-select --install
-else
-   /bin/bash ./roles/homebrew/files/install-homebrew.sh
-   type ansible >/dev/null 2>&1 || /usr/local/bin/brew install ansible
-fi
+ type brew >/dev/null 2>&1 || /bin/bash ./roles/homebrew/files/install-homebrew.sh
+ type ansible >/dev/null 2>&1 || /usr/local/bin/brew install ansible
