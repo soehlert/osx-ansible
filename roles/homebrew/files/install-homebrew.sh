d5128a168c74f86fa5c533b360d6799e63bfc1b9
Sam Oehlert <sam.oehlert@gmail.com>
2022-02-07 22:44:37 -0600

    refactor(makefile): set up makefile to have a useful help output

--- roles/homebrew/files/install-homebrew.sh
+++ roles/homebrew/files/install-homebrew.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-yes | /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+yes | /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
