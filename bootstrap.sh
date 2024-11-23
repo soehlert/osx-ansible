a2fd434b1b91df6138719895d9d2401eee3b2bc0
Sam Oehlert <sam.oehlert@gmail.com>
2024-11-22 21:51:27 -0600

    feat(galaxy): make sure to install the proper ansible-galaxy collection for pipx

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-# shell script to bootstrap the system to the point that it has ansible
- type brew >/dev/null 2>&1 || /bin/bash ./roles/homebrew/files/install-homebrew.sh
- type ansible >/dev/null 2>&1 || /usr/local/bin/brew install ansible
+type brew >/dev/null 2>&1 || /bin/bash ./roles/homebrew/files/install-homebrew.sh
+type ansible >/dev/null 2>&1 || /usr/local/bin/brew install ansible
+ansible-galaxy install -r requirements.yml
