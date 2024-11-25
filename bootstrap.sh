6ea72de302b62dd03da9fa1bf30365b5d5bf39ef
Sam Oehlert <sam.oehlert@gmail.com>
2024-11-25 00:50:22 -0600

    fix(updates): assume homebrew install prior and make sure we point to the right one

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-type brew >/dev/null 2>&1 || /bin/bash ./roles/homebrew/files/install-homebrew.sh
-type ansible >/dev/null 2>&1 || /usr/local/bin/brew install ansible
+eval "$(/opt/homebrew/bin/brew shellenv)"
+type ansible >/dev/null 2>&1 || brew install ansible
 ansible-galaxy install -r requirements.yml
