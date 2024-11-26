1968e870be1d5a187fc3689c93c2277c700c792c
Sam Oehlert <sam.oehlert@gmail.com>
2024-11-25 21:56:28 -0600

    ci(script): steal jeff geerling's script for uninstalling homebrew provided by github actions in favor of one we install

---
+++ tests/uninstall-homebrew.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#
+# Uninstalls Homebrew using the official uninstall script.
+
+# Download and run the uninstall script.
+curl -sLO https://raw.githubusercontent.com/Homebrew/install/master/uninstall.sh
+chmod +x ./uninstall.sh
+sudo ./uninstall.sh --force
+
+# Clean up Homebrew directories.
+sudo rm -rf /usr/local/Homebrew
+sudo rm -rf /usr/local/Caskroom
+sudo rm -rf /usr/local/bin/brew
+sudo rm -rf /opt/homebrew
